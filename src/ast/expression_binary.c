f2a50babb1de4f9d4190a22b20f9db50ff495566
aserjoker <aserjoker@outlook.com>
2026-03-01 21:13:42 +0800

    #feat #ptr

--- src/ast/expression_binary.c
+++ src/ast/expression_binary.c
@@ -686,6 +686,7 @@ cubec_ast_node_t cubec_read_ast_expression_binary_prefix(
       !cubec_location_is(node->opt->loc, "-") &&
       !cubec_location_is(node->opt->loc, "~") &&
       !cubec_location_is(node->opt->loc, "&") &&
+      !cubec_location_is(node->opt->loc, "*") &&
       !cubec_location_is(node->opt->loc, "try") &&
       !cubec_location_is(node->opt->loc, "typeof") &&
       !cubec_location_is(node->opt->loc, "sizeof")) {
