57b523c355dd3a9f4da6018a72933b1fe1168a26
aserjoker <aserjoker@outlook.com>
2026-04-15 10:17:59 +0800

    #feat #expression group

--- src/resolve/expression.c
+++ src/resolve/expression.c
@@ -4,6 +4,7 @@
 #include "eval/literal_identifier.h"
 #include "eval/literal_numeric.h"
 #include "eval/literal_string.h"
+#include "resolve/expression_group.h"
 value_t resolve_expression(context_t ctx, ast_node_t node) {
   if (node->type == NODE_TYPE_LITERAL_IDENTIFIER) {
     return eval_literal_identifier(ctx, node);
@@ -13,6 +14,8 @@ value_t resolve_expression(context_t ctx, ast_node_t node) {
     return eval_literal_string(ctx, node);
   } else if (node->type == NODE_TYPE_LITERAL_CHAR) {
     return eval_literal_char(ctx, node);
+  } else if (node->type == NODE_TYPE_EXPRESSION_GROUP) {
+    return resolve_expression_group(ctx, node);
   }
   return create_compile_error(ctx, node, "unsupport expression");
 }
