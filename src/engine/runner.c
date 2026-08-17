30fe755a0cf4641be4e04862327ed336bd8a2337
aserjoker <aserjoker@outlook.com>
2026-08-17 12:48:43 +0800

    refactor(engine): use create_void_value in run_program

--- src/engine/runner.c
+++ src/engine/runner.c
@@ -1,8 +1,6 @@
 #include "engine/runner.h"
 #include "engine/vm.h"
 #include "engine/value.h"
-#include "engine/interrupt_type.h"
-#include "engine/exception_type.h"
 #include "engine/void_type.h"
 #include "cubec/program.h"
 #include "cubec/node.h"
@@ -12,6 +10,5 @@ value_t run_program(vm_t vm, node_t node) {
   (void)vm;
   (void)node;
   /* TODO: iterate program->statements, run each statement */
-  return vm_create_value(vm, (type_t)value_get_data(vm_get_void_type(vm)), NULL,
-                         NULL);
+  return create_void_value(vm);
 }
